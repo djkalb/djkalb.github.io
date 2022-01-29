671c296e1ce63ca73e4a905e7a49a730e137618a
djkalb <71739290+djkalb@users.noreply.github.com>
2022-01-28 17:27:26 -0800

    added javascript to portfolio site

---
+++ Portfolio_Project/JS/portfolio.js
@@ -0,0 +1,37 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+
+}
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+ function showSlides(n) {
+     let slides = document.getElementsByClassName("mySlides")
+     let dots = document.getElementsByClassName("dot")
+     if (n > slides.length) {
+         slideIndex = 1
+     }
+     if (n < 1) {slideIndex = slides.length};
+     for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none"
+     }
+     for (i = 0; i < dots.length; i++) {
+         dots[i].className = dots[i].className.replace(" active", "")
+     }
+     slides[slideIndex - 1].style.display = "block";
+     dots[slideIndex - 1].className += " active";
+ }
+ document.addEventListener("click", (event) => {
+     if(event.target.matches("cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")) {
+         closeForm();
+     }
+ }, false)
